695596e28a94b4ee70df05f691ee7e39c190a33e
Hiếu Ngọc <nguyenhieu.hn213@gmail.com>
2023-05-14 22:49:41 +0700

    them nut themSV

--- BTTH01/index.php
+++ BTTH01/index.php
@@ -11,6 +11,9 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body class="container">
+    <div class="btn btn-primary my-3">
+        <a class="text-decoration-none text-white" href="create.php">Thêm Sinh Viên</a>
+    </div>
     <table class="table">
         <thead>
             <tr>
