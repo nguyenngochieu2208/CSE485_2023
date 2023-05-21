8ada83b7d5c35871dbb07d92e1b90296425a77a4
Vidiment <trqnthingocha2008@gmail.com>
2023-05-21 18:59:37 +0700

    update

---
+++ BTTH02/trangthai.php
@@ -0,0 +1,51 @@
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body class="container">
+    <div class="content">
+        <h1>Khu vực điểm danh</h1>
+    </div>
+
+    <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Ngày</th>
+            <th scope="col">Sự miêu tả</th>
+            <th scope="col">Trạng thái</th>
+            <th scope="col">Điểm</th>
+            </tr>
+        </thead>
+        <tbody>
+                <tr>
+                <td>
+                    <p>5/5/2023</p>
+                </td>
+                <td>
+                    <p>Buổi học bình thường</p>
+                </td>
+                <td>
+                    <p>Có mặt</p>
+                </td>
+                <td>
+                    <p>2/2</p>
+                </td>
+                </tr>
+        </tbody>
+    </table>
+
+
+    <table class="attlist mt-5">
+        <tbody>
+            <tr class="normal">
+                <td class="cell c0">Các buổi điểm danh:</td>
+                <td class="cell c1">1</td>
+            </tr>
+            <tr class="normal">
+                <td class="cell c0">Tỷ lệ điểm danh:</td>
+                <td class="cell c1 lastcol">100%</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
