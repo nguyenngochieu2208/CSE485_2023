e1be8d4b177286a474cec0142f5b2ad2154eae3f
Vidiment <trqnthingocha2008@gmail.com>
2023-05-21 15:54:52 +0700

    Hung

---
+++ BTTH02/dangki.php
@@ -0,0 +1,40 @@
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+
+<body>
+<h1 class="text">Register</h1>
+    <div class="login">
+        <div class="content">
+            <div class="img">
+                <img src="./img/website-ban-hang-1.png" alt="">
+            </div>
+        </div>
+        <div class="form">
+            <p class="form-title">Sign up to your account</p>
+            <div class="input-container">
+                <input type="name" placeholder="Enter your name">
+            </div>
+            <div class="input-container">
+                <input type="email" placeholder="Enter email">
+                <span>
+                </span>
+            </div>
+            <div class="input-container">
+                <input type="password" placeholder="Enter password">
+            </div>
+            <button type="submit" class="submit">
+                Sign up
+            </button>
+
+        </div>
+    </div>
+
+
+
+
+</body>
