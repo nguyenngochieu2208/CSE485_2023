9309b8d5873d09914c9ddf9035c68261f69f6c7b
Vidiment <trqnthingocha2008@gmail.com>
2023-05-21 22:25:36 +0700

    Update

--- BTTH02/templates/dangki.php
+++ BTTH02/templates/dangki.php
@@ -2,7 +2,7 @@
     <title>Register</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="../assets/css/log.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
 </head>
 
 
@@ -11,25 +11,14 @@
     <div class="login">
         <div class="content">
             <div class="img">
-<<<<<<< HEAD:BTTH02/dangki.php
-                <img src="./img/website-ban-hang-1.png" alt=""> 
+                <img src="../assets/images/anhLogin.png" alt=""> 
             </div>
         </div>
         <form class="form">
             <p class="form-title">Đăng kí tài khoản của bạn</p>
-=======
-                <img src="../assets/images/anhLogin.png" alt="">
-            </div>
-        </div>
-        <form class="form">
-
-            <p class="form-title">Sign up to your account</p>
-
->>>>>>> 935d2702a30eb284bb1e0ed51f44c75fae28c849:BTTH02/templates/dangki.php
             <div class="input-container">
                 <input type="name" name="Name" id="exampleInputName" placeholder="Vui lòng nhập tên">
             </div>
-
             <div class="input-container">
                 <input type="number" name="Number" id="exampleInputNumber" placeholder="Vui lòng nhập số">
             </div>
@@ -41,11 +30,9 @@
                 <span>
                 </span>
             </div>
-
             <div class="input-container">
                 <input type="password" name="Pass" id="exampleInputPassword" placeholder="Nhập mật khẩu">
             </div>
-            
             <button type="submit" class="submit" name="them">
                 Đăng kí
             </button>
--- BTTH02/templates/dangnhap.php
+++ BTTH02/templates/dangnhap.php
@@ -2,7 +2,7 @@
     <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="../assets/css/log.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
 </head>
 
 
@@ -15,39 +15,22 @@
             </div>
         </div>
         <form class="form">
-<<<<<<< HEAD:BTTH02/dangnhap.php
             <p class="form-title">Đăng nhập tài khoản của bạn</p>
             <div class="input-container">
                 <input type="email" name="Email" id="exampleInputEmail" placeholder="Nhập email">
-=======
-            <p class="form-title">Sign in to your account</p>
-
-            <div class="input-container">
-                <input type="email" name="email" id="exampleInputEmail" placeholder="Enter email">
->>>>>>> 935d2702a30eb284bb1e0ed51f44c75fae28c849:BTTH02/templates/dangnhap.php
                 <span>
                 </span>
             </div>
-
             <div class="input-container">
-<<<<<<< HEAD:BTTH02/dangnhap.php
                 <input type="password" name="Pass" id="exampleInputPassword" placeholder="Nhập password">
-=======
-                <input type="password" name="pass" id="exampleInputPassword" placeholder="Enter password">
->>>>>>> 935d2702a30eb284bb1e0ed51f44c75fae28c849:BTTH02/templates/dangnhap.php
             </div>
-
             <button type="submit" name="login" class="submit">
                 Đăng nhập
             </button>
 
             <p class="signup-link">
                 Chưa có tài khoản?
-<<<<<<< HEAD:BTTH02/dangnhap.php
                 <a href="">Đăng kí</a>
-=======
-                <a href="dangki.php">Đăng ký</a>
->>>>>>> 935d2702a30eb284bb1e0ed51f44c75fae28c849:BTTH02/templates/dangnhap.php
             </p>
         </form>
     </div>
