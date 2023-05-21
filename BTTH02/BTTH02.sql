f82c9a72fce6a54626f521b1258dc7403f43130c
hào lê <babi28102001@gmail.com>
2023-05-21 16:26:14 +0700

    BTTH2

--- BTTH02/BTTH02.sql
+++ BTTH02/BTTH02.sql
@@ -12,413 +12,92 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
 
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
-
--- Data exporting was unselected.
+-- Dumping database structure for btth2
+CREATE DATABASE IF NOT EXISTS `btth2` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `btth2`;
+
+-- Dumping structure for table btth2.giangvien
+CREATE TABLE IF NOT EXISTS `giangvien` (
+  `IDGiangVien` int(11) NOT NULL,
+  `Ten` varchar(100) NOT NULL,
+  `Email` varchar(100) NOT NULL DEFAULT '@',
+  `MatKhau` varchar(100) NOT NULL,
+  `SDT` bigint(100) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`IDGiangVien`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table btth2.khoahoc
+CREATE TABLE IF NOT EXISTS `khoahoc` (
+  `IDKhoaHoc` int(11) NOT NULL,
+  `MaKhoaHoc` varchar(50) NOT NULL,
+  `TieuDe` varchar(100) NOT NULL,
+  `MoTa` varchar(1000) NOT NULL,
+  PRIMARY KEY (`IDKhoaHoc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table btth2.lop
+CREATE TABLE IF NOT EXISTS `lop` (
+  `IDLop` int(11) NOT NULL,
+  `IDKhoaHoc` int(11) NOT NULL,
+  `IDGiangVien` int(11) NOT NULL,
+  `KhoangThoiGia` datetime NOT NULL,
+  PRIMARY KEY (`IDLop`),
+  KEY `IDGiangVien` (`IDGiangVien`),
+  KEY `FK_lop_khoahoc` (`IDKhoaHoc`),
+  CONSTRAINT `FK_lop_khoahoc` FOREIGN KEY (`IDKhoaHoc`) REFERENCES `khoahoc` (`IDKhoaHoc`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `IDGiangVien` FOREIGN KEY (`IDGiangVien`) REFERENCES `giangvien` (`IDGiangVien`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table btth2.quantrivien
+CREATE TABLE IF NOT EXISTS `quantrivien` (
+  `IDQtv` int(11) NOT NULL,
+  `Ten` varchar(100) NOT NULL,
+  `MatKhau` varchar(100) NOT NULL,
+  PRIMARY KEY (`IDQtv`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table btth2.sinhvien
+CREATE TABLE IF NOT EXISTS `sinhvien` (
+  `IDSinhVien` int(11) NOT NULL,
+  `Ten` varchar(100) NOT NULL,
+  `NgaySinh` date NOT NULL,
+  `Email` varchar(50) NOT NULL DEFAULT '@',
+  `SDT` int(11) NOT NULL,
+  `IDKhoaHoc` int(11) NOT NULL,
+  `TenKhoaHoc` varchar(100) NOT NULL,
+  `GiaDoan` int(11) NOT NULL,
+  `HocKy` int(11) NOT NULL,
+  `MatKhau` varchar(100) NOT NULL,
+  PRIMARY KEY (`IDSinhVien`),
+  KEY `IDKhoaHoc` (`IDKhoaHoc`),
+  CONSTRAINT `IDKhoaHoc` FOREIGN KEY (`IDKhoaHoc`) REFERENCES `khoahoc` (`IDKhoaHoc`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table btth2.suthamdu
+CREATE TABLE IF NOT EXISTS `suthamdu` (
+  `NgayThamDu` datetime DEFAULT NULL,
+  `IDKhoaHoc` int(11) NOT NULL,
+  `IDSinhVien` int(11) NOT NULL,
+  `IDLop` int(11) NOT NULL,
+  `TrangThaiThamDu` int(11) NOT NULL,
+  KEY `FK_suthamdu_khoahoc` (`IDKhoaHoc`),
+  KEY `FK_suthamdu_sinhvien` (`IDSinhVien`),
+  KEY `FK_suthamdu_lop` (`IDLop`),
+  CONSTRAINT `FK_suthamdu_khoahoc` FOREIGN KEY (`IDKhoaHoc`) REFERENCES `khoahoc` (`IDKhoaHoc`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_suthamdu_lop` FOREIGN KEY (`IDLop`) REFERENCES `lop` (`IDLop`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_suthamdu_sinhvien` FOREIGN KEY (`IDSinhVien`) REFERENCES `sinhvien` (`IDSinhVien`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Data exporting was unselected.
 
