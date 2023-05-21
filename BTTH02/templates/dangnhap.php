935d2702a30eb284bb1e0ed51f44c75fae28c849
Hiếu Ngọc <nguyenhieu.hn213@gmail.com>
2023-05-21 16:42:26 +0700

    to chuc thu muc

--- BTTH02/templates/dangnhap.php
+++ BTTH02/templates/dangnhap.php
@@ -1,36 +1,40 @@
 <head>
-    <title>Register</title>
+    <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="../assets/css/log.css">
 </head>
 
 
 <body>
-<h1 class="text">Register</h1>
+    <h1 class="text">Login</h1>
     <div class="login">
         <div class="content">
             <div class="img">
-                <img src="./img/website-ban-hang-1.png" alt="">
+                <img src="../assets/images/anhLogin.png" alt="">
             </div>
         </div>
         <form class="form">
-            <p class="form-title">Sign up to your account</p>
+            <p class="form-title">Sign in to your account</p>
+
             <div class="input-container">
-                <input type="name" name="Name" id="exampleInputName" placeholder="Enter your name">
-            </div>
-            <div class="input-container">
-                <input type="email" name="Email" id="exampleInputEmail1" placeholder="Enter email">
+                <input type="email" name="email" id="exampleInputEmail" placeholder="Enter email">
                 <span>
                 </span>
             </div>
+
             <div class="input-container">
-                <input type="password" name="Pass" id="exampleInputPassword" placeholder="Enter password">
+                <input type="password" name="pass" id="exampleInputPassword" placeholder="Enter password">
             </div>
-            <button type="submit" class="submit" name="them">
-                Sign up
+
+            <button type="submit" name="login" class="submit">
+                Đăng nhập
             </button>
 
+            <p class="signup-link">
+                Chưa có tài khoản?
+                <a href="dangki.php">Đăng ký</a>
+            </p>
         </form>
     </div>
 
